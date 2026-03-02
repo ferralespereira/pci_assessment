47ce603603f1b7568588a95c2d952b3e92590e4d
javier <ferralespereira@gmail.com>
2026-03-02 14:24:51 -0500

    pci_assessment project

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Button, Alert, Navbar, Container, Nav } from 'react-bootstrap';
+
+function App(): JSX.Element {
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">MyApp</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Container className="mt-5">
+        <Alert variant="success">
+          Welcome to React-Bootstrap! 🎉
+        </Alert>
+        <Button variant="primary">Click Me</Button>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+// import './index.css';
+
+// const root = ReactDOM.createRoot(document.getElementById('root')!);
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
